5dabd95115da42cacd076cbda37c45cd51027517
kidakorn-workplace <kidakorn.lik@student.mahidol.edu>
2026-02-02 09:50:18 +0000

    fff

--- main.c
+++ main.c
@@ -7,114 +7,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "node.h"
 
 int main(int argc, const char * argv[]) {
-    int c=5;
-    struct node a,b,*head ;
-    a.value = c;
-    a.next=&b;
-    head=&a;
-    b.value=head->value+3;
-
-    struct node d;
-    b.next =&d;
-    d.value=11;
-    d.next=NULL;
-
-    printf("%d\n", head ->value ); //what value for 5
-    printf("%d\n", head ->next->value ); //what value for 8
-    printf("%d\n", head ->next->next->value ); //what value for 11
-   
-/*  Exercise I
-    1. Add 1 more than at the end
-    2. Add value(11)
-    3. Make next become NULL
- */
-
- printf("insert in front\n");
- struct node e;//start at 2
-    e.value=2;
-    e.next=&a;
-    head=&e;    
-
-
- struct node f;
- f.value=7;
-    f.next=&b;
-    a.next=&f;
- printf("%d\n", head ->value ); //what value for 2
-    printf("%d\n", head ->next->value ); //what value for 5
-    printf("%d\n", head ->next->next->value ); //what value for 7
-    printf("%d\n", head ->next->next->next->value ); //what value for 8
-    printf("%d\n", head ->next->next->next->next->value ); //what value for 11
-   
-/*  Exercise II
-        1. Add 1 more than at the begining!!!!
-        2. Add value (2)
-        
-*/
-    typedef struct node* NodePtr;
-    NodePtr tmp=head; //add temp value to faciliate
-        printf("print using loop\n");
-        //use loop to print all value
-        int i,n=5;
-        for(i=0;i<n;i++){
-            printf("%3d", tmp->value);
-            tmp=tmp->next; 
-
-        }
-        printf("\n");   
-        
-    /*  Exercise III Use loop to print everything
-        int i,n=5;
-        for(i=0;i<n;i++){
-            printf("%3d", tmp->value);
-          // What is missing???
-        }
-    */
-    
-   /*  Exercise IV change to while loop!! (you can use NULL to help) */
-       
-         while(tmp!=NULL){
-            printf("%3d", tmp->value);
-            tmp=tmp->next; // What is missing???
-        }
-    
-    
- /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
-         //use a loop to help
-     */
-        printf("\nCreate all nodes using malloc\n");
-        typedef struct node* NodePtr;
-        NodePtr temp;
-        head=(NodePtr) malloc (sizeof(struct node));
-        temp=head;
-        n=5;
-        for(i=0;i<n;i++){
-            temp->value=7+i*2; // 5,7,9,11,13
-            temp->next=(NodePtr) malloc (sizeof(struct node));
-            temp=temp->next;
-            
-        }
-        temp->value=7+i*2; //terminate value
-        temp->next=NULL; //terminate next
-
-        //printf("print using while loop\n");
-        temp=head;
-        while(temp!=NULL){
-            printf("%3d", temp->value);
-            temp=temp->next; // What is missing???
-            printf("\n");   
-            
-        }
-
-        // Free all nodes
-        while(head != NULL) {
-            NodePtr temp = head;
-            head = head->next;
-            free(temp);
-        }
-    return 0;
+    if (argc < 3 || ((argc - 1) % 2) != 0) {
+        fprintf(stderr, "Usage: %s id name [id name]...\n", argv[0]);
+        return 1;
     }
+
+    typedef struct student Student;
+    Student *head = NULL, *tail = NULL;
+
+    for (int i = 1; i < argc; i += 2) {
+        int id = atoi(argv[i]);
+        const char *name = argv[i + 1];
+
+        Student *n = malloc(sizeof *n);
+        if (!n) { perror("malloc"); return 1; }
+        n->id = id;
+        strncpy(n->name, name, NAME_MAX - 1);
+        n->name[NAME_MAX - 1] = '\0';
+        n->next = NULL;
+
+        if (!head) head = tail = n;
+        else { tail->next = n; tail = n; }
+    }
+
+    /* Print all data in the linked list */
+    printf("Linked list contents:\n");
+    Student *cur = head;
+    while (cur) {
+        printf("id=%d name=%s\n", cur->id, cur->name);
+        cur = cur->next;
+    }
+
+    /* Delete / free all linked list nodes */
+    cur = head;
+    while (cur) {
+        Student *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+
+    return 0;
+}
 
